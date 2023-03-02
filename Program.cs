d699f322b64426f349674319874179dffb5bfbc2
asouzaaa <souza_amandaa@icloud.com>
2023-03-02 14:22:17 -0300

    segundo exercicio

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+    class Program //Contador de Vogais
+    {
+        static void Main(string[] args)
+        {
+            int vogal = 0;
+            char[] caracteres = { 'a', 'e', 'i', 'o', 'u' };
+            
+            Console.WriteLine("Digite uma palavra: ");
+
+            char[] frase_completa = Console.ReadLine().ToLower().ToCharArray();
+
+            for (int conta = 0; conta < frase_completa.Length; conta++)
+            {
+                if (caracteres.Contains(frase_completa[conta]))
+                {
+                    vogal++;
+                }
+            }
+            Console.WriteLine("Total de {0} vogais ", vogal, caracteres[0]);
+            Console.Read();
+
+            Console.ReadKey();
+        }
+        }
+}
